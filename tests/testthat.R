c30b226316b7d1be31316d836bf50f8d07179147
Gregir121 <grzesniego@gmail.com>
2026-04-24 14:54:08 +0200

    Path handling in testhat file

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,9 @@
 library(testthat)
-source("R/api_functions.R")
-test_dir("testthat")
+
+if (file.exists("R/api_functions.R")) {
+  source("R/api_functions.R")
+} else if (file.exists("api_functions.R")) {
+  source("api_functions.R")
+}
+
+test_dir("tests/testthat")
